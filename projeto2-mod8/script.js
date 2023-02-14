67af91be9929a39af0ed6da63e31d66e80fea3c0
Mônica Picaço <movioloncelo@gmail.com>
2023-02-14 19:34:01 -0300

    Finalizei des2 mod8

--- projeto2-mod8/script.js
+++ projeto2-mod8/script.js
@@ -1,21 +1,38 @@
 let nome = document.getElementById('name')
 let telefone = document.getElementById('number')
+cadastrados = []
 let linhas = ''
 
+function resultado() {
+    let ultLinha = '<tr>'
+    ultLinha += `<td><strong>Adicionados</strong></td>`
+    ultLinha += `<td><strong>${cadastrados.length}</strong></td>`
+    ultLinha += '</tr>'
+
+    let footTabela = document.querySelector('tfoot')
+    footTabela.innerHTML = ultLinha
+}
+
 function adicionar() {
+    if (nome.value.length == 0 || telefone.value.length == 0) {
+        alert('Digite um contato para adicionar')
+    } else {
+        let linha = '<tr>'
+        linha += `<td>${nome.value}</td>`
+        linha += `<td>${telefone.value}</td>`
+        linha += '</tr>'
 
-    let linha = '<tr>'
-    linha += `<td>${nome.value}</td>`
-    linha += `<td>${telefone.value}</td>`
-    linha += '</tr>'
+        linhas += linha
+        
+        let corpoTabela = document.querySelector('tbody')
+        corpoTabela.innerHTML = linhas
+        cadastrados.push(nome.value)
 
-    linhas += linha
-    
-    let corpoTabela = document.querySelector('tbody')
-    corpoTabela.innerHTML = linhas
+        nome.value = ''
+        telefone.value = ''
+        nome.focus()
 
-    nome.value = ''
-    telefone.value = ''
-    nome.focus()
+        resultado()
+    }
 }
 
