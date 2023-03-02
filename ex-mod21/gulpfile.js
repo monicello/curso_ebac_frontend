938b772a7d20214f24131bb691f312165bfac5dd
Mônica Picaço <movioloncelo@gmail.com>
2023-03-02 20:26:37 -0300

    Iniciando o projeto

---
+++ ex-mod21/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = gulp.parallel(styles);
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    // gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+}
