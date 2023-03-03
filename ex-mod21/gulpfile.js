56c1f2298e275fecc51a1f07b22d8433a1e3f6bf
Mônica Picaço <movioloncelo@gmail.com>
2023-03-03 20:45:56 -0300

    up

--- ex-mod21/gulpfile.js
+++ ex-mod21/gulpfile.js
@@ -3,14 +3,27 @@ const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
 const uglify = require('gulp-uglify')
 
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
+
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles);
+function images() {
+    return gulp.src('./src/imagens/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    // gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
