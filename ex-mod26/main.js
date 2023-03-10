a79a858c6a5e85ce363cf58a7efbeae7d5c671f9
Mônica Picaço <movioloncelo@gmail.com>
2023-03-10 20:57:50 -0300

    Finalizei ex-mod26

---
+++ ex-mod26/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplica(num1, num2) {
+    return num1 * num2;
+}
+function dizOla(nome) {
+    return `Olá, ${nome}`;
+}
+let resultado = multiplica(10, 10);
+let saudacao = dizOla('Ana');
+console.log(resultado);
+console.log(saudacao);
---
+++ ex-mod26/main.ts
@@ -0,0 +1,13 @@
+function multiplica(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function dizOla(nome: string): string {
+    return `Olá, ${nome}`;
+}
+
+let resultado = multiplica(10, 10);
+let saudacao = dizOla('Ana');
+
+console.log(resultado)
+console.log(saudacao)
